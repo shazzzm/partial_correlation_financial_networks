4582c6add97f06e3dd3e9fbb68e5429f6d276be1
Tristan Millington <tristan.millington@gmail.com>
2019-10-21 10:33:04 +0100

    Added modularity stuff

---
+++ louvain_cython.pyx
@@ -0,0 +1,460 @@
+import numpy as np
+cimport numpy as np
+import networkx as nx
+DTYPE = np.double
+ctypedef np.double_t DTYPE_t
+ctypedef np.int_t DTYPE_int
+
+def modularity_classic(np.ndarray[DTYPE_t, ndim=2] M, np.ndarray[DTYPE_int, ndim=1] assignments):
+    """
+    Calculates the modularity of an unsigned weighted network
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix representing the graph
+    assignments : array_like
+        p length vector containing node community assignments
+    Returns
+    -------
+    modularity : float
+        Value of the modularity
+    """
+    cdef int p = M.shape[0]
+    K = M.sum(axis=0)
+    cdef float Q = 0
+    cdef float C_norm = M.sum()
+    for i in range(p):
+        for j in range(p):
+            if assignments[i] != assignments[j]:
+                continue
+
+            Q += M[i,j] - ((K[i] * K[j])/C_norm)
+
+    return Q/(C_norm)
+
+
+def modularity_signed(np.ndarray[DTYPE_t, ndim=2] M, np.ndarray[DTYPE_int, ndim=1] assignments):
+    """
+    Calculates the modularity of a signed weighted network
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix representing the graph
+    assignments : array_like
+        p length vector containing node community assignments
+    Returns
+    -------
+    modularity : float
+        Value of the modularity
+    """
+    p = M.shape[0]
+    M_pos = M.copy()
+    M_pos[M_pos < 0] = 0
+    M_neg = M.copy()
+    M_neg[M_neg > 0] = 0
+    M_neg = - M_neg
+
+    K_pos = M_pos.sum(axis=0)
+    K_neg = M_neg.sum(axis=0)
+
+    tot_pos = M_pos.sum()
+    tot_neg = M_neg.sum()
+
+    Q_pos = 0
+    if tot_pos > 0:
+        for i in range(p):
+            for j in range(p):
+                if assignments[i] == assignments[j]:
+                    Q_pos += M_pos[i, j] - (K_pos[i] * K_pos[j]/tot_pos)
+
+    Q_neg = 0
+    if tot_neg > 0:
+        for i in range(p):
+            for j in range(p):
+                if assignments[i] == assignments[j]:
+                    Q_neg += M_neg[i, j] - (K_neg[i] * K_neg[j]/tot_neg)
+
+
+    Q = tot_pos / (tot_pos + tot_neg) * Q_pos + tot_neg / (tot_pos + tot_neg) * Q_neg
+
+    return Q
+
+def modularity_diff_correlation(np.ndarray[DTYPE_t, ndim=2] M, int i, np.ndarray[DTYPE_int, ndim=1] assignments, int community):
+    """
+    Calculates the gain in modularity of taking node i from an isolated community
+    into the community specified for the null model of a correlation
+    network
+
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix representing the graph
+    i : integer
+        index of the node being moved
+    assignments : array_like
+        p length vector containing node community assignments
+    community : integer
+        index of the community node i is being moved into
+    Returns
+    -------
+    modularity_diff : float
+        change in modularity
+    Notes
+    -----
+    i must not be assigned to a community in the community vector - assign it to -1 community
+    """
+    ind = assignments == community
+    m = M.sum()
+    # Avoid a NaN
+    if m == 0:
+        return 0
+    return M[i, ind].sum()#/m
+
+def modularity_correlation(np.ndarray[DTYPE_t, ndim=2] M, np.ndarray[DTYPE_int, ndim=1] assignments):
+    """
+    Calculates the modularity of a correlation network
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix representing the graph
+    assignments : array_like
+        p length vector containing node community assignments
+    Returns
+    -------
+    modularity : float
+        Value of the modularity
+    """
+    cdef int p = M.shape[0]
+    cdef float Q = 0
+    cdef float C_norm = M.sum()
+    for i in range(p):
+        for j in range(p):
+            if i == j:
+                val = 1
+            else:
+                val = 0
+            if assignments[i] != assignments[j]:
+                continue
+
+            Q += M[i,j] - val 
+
+    return Q/(C_norm)
+
+def modularity_market_mode(np.ndarray[DTYPE_t, ndim=2] M, np.ndarray[DTYPE_int, ndim=1] assignments):
+    """
+    Calculates the modularity of a correlation network with the presence of a market mode
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix representing the graph
+    assignments : array_like
+        p length vector containing node community assignments
+    Returns
+    -------
+    modularity : float
+        Value of the modularity
+    """
+    cdef int p = M.shape[0]
+    cdef float Q = 0
+    cdef float C_norm = M.sum()
+
+    eigs, eigv = np.linalg.eig(M)
+    max_ind = eigs.argmax()
+    C_mode = eigs[max_ind] * eigv[max_ind] @ eigv[max_ind].T
+    for i in range(p):
+        for j in range(p):
+            if i == j:
+                val = 1
+            else:
+                val = 0
+            if assignments[i] != assignments[j]:
+                continue
+
+            Q += M[i,j] - C_mode[i, j] - val 
+
+    return Q/(C_norm)
+
+
+def modularity_diff(np.ndarray[DTYPE_t, ndim=2] M, int i, np.ndarray[DTYPE_int, ndim=1] assignments, int community):
+    """
+    Calculates the gain in modularity of taking node i from an isolated community into
+    the community specified for an unsigned weighted graph
+
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix representing the graph
+    i : integer
+        index of the node being moved
+    assignments : array_like
+        p length vector containing node community assignments
+    community : integer
+        index of the community node i is being moved into
+    Returns
+    -------
+    modularity_diff : float
+        change in modularity
+    Notes
+    -----
+    i must not be assigned to a community in the community vector - assign it to -1 community
+    """
+    ind = assignments == community
+    m = M.sum()
+    # Avoid a NaN
+    if m == 0:
+        return 0
+
+    sum_in = M[ind, :][:, ind].sum()/2
+    sum_tot = M[ind, :].sum()
+    k_i = M[i, :].sum()
+    k_in = M[i, ind].sum()
+    return (sum_in + 2 * k_in)/(m) - ((sum_tot + k_i)/(m))**2 - sum_in/(m) + (sum_tot/(m))**2 + (k_i/(m))**2
+
+def modularity_diff_signed(np.ndarray[DTYPE_t, ndim=2] M_pos, np.ndarray[DTYPE_t, ndim=2] M_neg, int i, np.ndarray[DTYPE_int, ndim=1] assignments, int community):
+    """
+    Calculates the gain in modularity of taking node i from an isolated community into
+    the community specified for a signed weighted graph
+
+    Parameters
+    ----------
+    M_pos : array_like
+        p by p adjacency matrix representing the positive edges in the graph
+    i : integer
+        index of the node being moved
+    assignments : array_like
+        p length vector containing node community assignments
+    community : integer
+        index of the community node i is being moved into
+    Returns
+    -------
+    modularity_diff : float
+        change in modularity
+    Notes
+    -----
+    i must not be assigned to a community in the community vector - assign it to -1 community
+    """
+    pos_gain = modularity_diff(M_pos, i, assignments, community)
+    neg_gain = modularity_diff(M_neg, i, assignments, community)
+
+    w_pos = M_pos.sum()
+    w_neg = M_neg.sum()
+
+    return (w_pos / (w_pos + w_neg)) * pos_gain - (w_neg / (w_pos + w_neg)) * neg_gain
+
+def run_one_level(np.ndarray[DTYPE_t, ndim=2] M, int signed=False, int correlation=False):
+    """
+    Runs the first phase of the Louvain community detection algorithm for a weighted graph, returns a set of assignments
+    for each node of the graph. 
+
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix representing the graph
+    signed : bool (optional, default=False)
+        If the graph is signed or not
+    correlation : bool (optional, default=False)
+        If the graph is a correlation network
+    Returns
+    -------
+    assignments : array_like
+        p length vector with what community a node has been assigned to
+    """
+    cdef int p = M.shape[0]
+    assignments = np.arange(p)
+    communities = set(range(p))
+    #run = True
+    # we use this to count how many nodes we've gone without
+    # updating - if we cycle all the way round we probably can't
+    # improve anymore
+    cdef int no_not_updated = 0
+    #nodes = 
+    modified = True
+
+    if signed:
+        pos_ind = M > 0
+        neg_ind = M < 0
+        M_pos = M.copy()
+        M_pos[neg_ind] = 0
+        M_neg = M.copy()
+        M_neg[pos_ind] = 0
+        M_neg[neg_ind] = -M_neg[neg_ind]
+
+    # This stores whether we need to recalculate modularity for the network
+    # i.e. has something changed on that run through round
+    while modified:
+        modified = False
+        nodes = np.random.choice(p, size=p)
+        for i in range(p):
+            ind = nodes[i]
+            max_diff_i = -1
+            max_diff = 0
+            connected_to = M[ind, :] != 0
+            communities_to_consider_set = list(set(assignments[connected_to]))
+            communities_to_consider = np.random.choice(communities_to_consider_set, size=len(communities_to_consider_set))
+
+            old_com = assignments[ind]
+
+            # Remove the node from it's community
+            assignments[ind] = -1
+
+            if signed:
+                removal_cost = -modularity_diff_signed(M_pos, M_neg, ind, assignments, old_com)
+            elif correlation:
+                removal_cost = -modularity_diff_correlation(M, ind, assignments, old_com)
+            else:
+                removal_cost = -modularity_diff(M, ind, assignments, old_com)
+                #assignments[ind] = old_com
+                #old_mod = modularity_signed(M, assignments)
+                #assignments[ind] = -1
+
+            run_through_modified = False
+            for com in communities_to_consider:
+                if signed:
+                    change = modularity_diff_signed(M_pos, M_neg, ind, assignments, com)
+                elif correlation:
+                    change = modularity_diff_correlation(M, ind, assignments, com)
+                else:
+                    change = modularity_diff(M, ind, assignments, com)
+
+                diff = change + removal_cost
+                if max_diff < diff:
+                    max_diff = diff
+                    max_diff_i = com
+            if max_diff > 0 and max_diff_i != -1:
+                no_not_updated = 0
+                modified = True
+                assignments[ind] = max_diff_i
+            else:
+                # If there isn't a better community to put it in, put it back
+                no_not_updated += 1
+                assignments[ind] = old_com
+
+    communites = {com:i for i,com in enumerate(set(assignments))}
+
+    # remap communities into a range from 0-number of communities
+    for i in range(p):
+        assignments[i] = communites[assignments[i]]
+
+    return assignments
+
+def induced_graph(np.ndarray[DTYPE_t, ndim=2] M, np.ndarray[DTYPE_int, ndim=1] assignments, labels, first):
+    """
+    Folds all the communities into their own node - phase 2 of the Louvain algorithm
+
+    Parameters
+    ----------
+    M : array_like
+        p by p adjacency matrix of the graph
+    assignments : array_like
+        p length vector containing node community assignments
+    labels : either a list if this is the first run or a dictionary if we've already
+             folded a network in
+        Contains what nodes the communities correspond to if we've already run the 
+        algorithm once. This bit isn't pretty.
+    first : bool
+        If this is the first run through
+    Returns
+        tuple (new_M, folded)
+
+        new_M contains the new adjacency matrix and folded contains a dictionary 
+        which nodes each community belongs to
+    """
+    p = M.shape[0]
+    communities = list(set(assignments))
+    no_communities = len(communities)
+    new_M = np.zeros((no_communities, no_communities))
+    # Folded contains the nodes that the community contains
+    folded = {}
+    if first:
+        labels = np.array(labels)
+    for i,com in enumerate(communities):
+        ind = assignments == com
+        # Get all the connections within the community
+        self_weight = M[ind, :][:, ind].sum()
+        new_M[i, i] = self_weight
+        if first:
+            labels = np.array(labels)
+            folded[i] = set(labels[np.where(ind)[0]])
+        else:
+            folded[i] = set()
+            nodes_in_community = np.where(ind)[0]
+            for node in nodes_in_community:
+                folded[i] = folded[i] | labels[node]
+        for j,com_2 in enumerate(communities):
+            if com == com_2:
+                continue
+            ind_2 = assignments == com_2
+            #M_com = M.copy()
+            M_com = M[ind, :]
+            M_com = M_com[:, ind_2]
+            weight = M_com.sum()
+            new_M[j, i] = weight
+            new_M[i, j] = weight
+
+    return new_M, folded
+    
+def run_louvain_nx(G, nodes=None, int max_iter=100, int signed=False, int correlation=False):
+    """
+    Runs the Louvain community detection algorithm on a networkx graph
+    Parameters
+    ----------
+    G : networkx graph
+        Graph to run the algorithm on
+    max_iter : int (optional, default=5)
+        Maximum number of iterations of the algorithm to run
+    a : int (optional, default=1)
+        Description of what a does
+    signed : bool (optional, default=False)
+        If the graph is signed or not
+    correlation : bool (optinal, default=False)
+        If the graph is a correlation network
+
+    Returns
+        tuple (dict, dict)
+        First dict contains the best possible community assignments
+        Second dict contains the entire output if you wish to resolve 
+        communities of multiple scales
+    """
+    assignments_dct = {}
+    i = 0
+    new_G = G.copy()
+    old_mod = -np.inf
+    assignments_dct = {}
+
+    if correlation and signed:
+        raise ValueError("Both correlation and signed cannot be true")
+
+    if nodes is None:
+        node_labels = list(G.nodes())
+    else:
+        node_labels = nodes
+
+    while True:
+        M = nx.to_numpy_array(new_G)#, nodelist=nodes)
+
+        assignments = run_one_level(M, signed=signed, correlation=correlation)
+        # Fold them into an induced graph
+
+        M, node_labels = induced_graph(M, assignments, node_labels, i==0)
+        if signed:
+            mod = modularity_signed(M, assignments)
+        elif correlation:
+            mod = modularity_correlation(M, assignments)
+        else:
+            mod = modularity_classic(M, assignments)
+        new_G = nx.from_numpy_array(M)
+        #new_G = nx.relabel_nodes(new_G, folded)
+        i += 1
+        print(i)
+        print(mod)
+        assignments_dct[i] = node_labels
+        # Quit if we can't increase the modularity
+        # or if we've run out of iterations
+        # or if there is one giant community
+        if mod <= old_mod: #or i > max_iter: #or len(set(node_labels))==1:
+            break
+        old_mod = mod
+
+    return assignments_dct[i-1], assignments_dct
+
+
---
+++ modularity_over_time.py
@@ -0,0 +1,247 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import math
+import networkx as nx
+import os
+import pandas as pd
+import seaborn as sns
+from pathlib import Path
+import operator
+import matplotlib
+import louvain_cython as lcn
+from sklearn.metrics import adjusted_rand_score
+
+def sort_dict(dct):
+    """
+    Takes a dict and returns a sorted list of key value pairs
+    """
+    sorted_x = sorted(dct.items(), key=operator.itemgetter(1), reverse=True)
+
+    return sorted_x
+
+def save_open_figures(prefix=""):
+    """
+    Saves all open figures
+    """
+    figures=[manager.canvas.figure
+         for manager in matplotlib._pylab_helpers.Gcf.get_all_fig_managers()]
+
+    for i, figure in enumerate(figures):
+        mng = plt.get_current_fig_manager()
+        mng.resize(*mng.window.maxsize())
+        figure.savefig(prefix+'figure%d.png' % i)
+
+def get_sector_full_nice_name(sector):
+    """
+    Returns a short version of the sector name
+    """       
+    if sector == "information_technology":
+        return "Information Technology"
+    elif sector == "real_estate":
+        return "Real Estate"
+    elif sector == "materials":
+        return "Materials"
+    elif sector == "telecommunication_services":
+        return "Telecommunication Services"
+    elif sector == "energy":
+        return "Energy"
+    elif sector == "financials":
+        return "Financials"
+    elif sector == "utilities":
+        return "Utilities"
+    elif sector == "industrials":
+        return "Industrials"
+    elif sector == "consumer_discretionary":
+        return "Consumer Discretionary"
+    elif sector == "health_care":
+        return "Healthcare"
+    elif sector == "consumer_staples":
+        return "Consumer Staples"
+    else:
+        raise Exception("%s is not a valid sector" % sector)
+
+def plot_bar_chart(vals, label=None, title=None, xlabel=None, ylabel=None):
+    fig = plt.figure()
+    n = vals.shape[0]
+    index = np.arange(n)
+    bar_width = 0.1
+    rects1 = plt.bar(index, vals, bar_width, label=label)
+    #axes = fig.axes
+    #print(axes)
+    #axes[0].set_xticklabels(label)
+    plt.xticks(index, label)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+def min_max_normalization(df):
+    return (df - df.min())/(df.max() - df.min())
+
+def compare_cluster_consistency(current_assignments, previous_assignments):
+    rand_index = []
+    for cur_assignment in curr_assignments:
+        for prev_assignment in previous_assignments:
+            rand_index.append(adjusted_rand_score(cur_assignment, prev_assignment))
+
+    rand_index = np.array(rand_index)
+    return np.mean(rand_index), np.std(rand_index), rand_index
+
+np.seterr(all='raise')
+
+#df = pd.DataFrame.from_csv("s_and_p_500_sector_tagged.csv")
+df = pd.read_csv("s_and_p_500_daily_close_filtered.csv", index_col=0)
+company_sectors = df.iloc[0, :].values
+company_names = df.T.index.values
+sectors = list(sorted(set(company_sectors)))
+num_sectors = len(sectors)
+company_sector_lookup = {}
+
+for i,comp in enumerate(company_names):
+    company_sector_lookup[comp] = company_sectors[i]
+
+df_2 = df.iloc[1:, :]
+df_2 = df_2.apply(pd.to_numeric)
+df_2 = np.log(df_2) - np.log(df_2.shift(1))
+X = df_2.values[1:, :]
+
+num_runs_community_detection = 10
+
+window_size = 300
+slide_size = 30
+no_samples = X.shape[0]
+p = X.shape[1]
+no_runs = math.floor((no_samples - window_size)/ (slide_size))
+dates = []
+
+for x in range(no_runs-1):
+    dates.append(df.index[(x+2)*slide_size+window_size][0:10])
+
+dates_2 = []
+
+for x in range(no_runs):
+    dates_2.append(df.index[(x+1)*slide_size+window_size][0:10])
+
+networks_folder = "networks_lw/"
+onlyfiles = [os.path.abspath(os.path.join(networks_folder, f)) for f in os.listdir(networks_folder) if os.path.isfile(os.path.join(networks_folder, f))]
+#onlyfiles = list(map(lambda x: os.path.splitext(x)[0], onlyfiles))
+Graphs = []
+
+# Sort the files into order
+ind = [int(Path(x).stem[23:]) for x in onlyfiles]
+ind = np.argsort(np.array(ind))
+
+for i in ind:
+    f = onlyfiles[i]
+    G = nx.read_graphml(f)
+    Graphs.append(G)
+
+
+max_eigs = np.zeros(no_runs)
+
+sectors = set()
+for node in G.nodes:
+    sectors.add(G.nodes[node]['sector'])
+no_communities = len(sectors)
+sectors = list(sectors)
+sectors = {sec:i for i,sec in enumerate(sectors)}
+
+sector_assignments_true = np.zeros(len(G.nodes))
+rand_scores_all = np.zeros((no_runs, num_runs_community_detection))
+rand_scores_mean = []
+rand_scores_stdev = []
+
+cluster_consistency_all = np.zeros((no_runs, num_runs_community_detection**2))
+cluster_consistency_mean = []
+cluster_consistency_stdev = []
+
+for i,node in enumerate(G.nodes):
+    sector_assignments_true[i] = sectors[G.nodes[node]['sector']]
+
+prev_assigments = []
+
+number_clusters_all = np.zeros((no_runs, num_runs_community_detection))
+number_of_clusters_mean = []
+number_of_clusters_stdev = []
+
+nodes = list(G.nodes())
+
+node_assignments = np.chararray(p, itemsize=5)
+
+for i in range(p):
+    node_assignments[i] = nodes[i]
+
+np.save("node_assignments", node_assignments)
+assignments_overall = np.zeros((no_runs, len(G.nodes), num_runs_community_detection))
+
+for i,G in enumerate(Graphs):
+    print("Running %s" % i)
+    rand_scores = np.zeros(num_runs_community_detection)
+    curr_assignments = []
+    num_clusters = np.zeros(num_runs_community_detection)
+    for run in range(num_runs_community_detection):
+        communities, assignments_dct = lcn.run_louvain_nx(G, nodes, signed=True)
+        assignments = np.zeros(len(G.nodes))
+        nodes = list(G.nodes)
+        for j,com in enumerate(communities):
+            for node in communities[com]:
+                assignments[nodes.index(node)] = j
+        score = adjusted_rand_score(sector_assignments_true, assignments)
+        rand_scores[run] = score
+        curr_assignments.append(assignments)
+        num_clusters[run] = len(set(assignments))
+        assignments_overall[i, :, run] = assignments
+
+    number_clusters_all[i, :] = num_clusters
+    number_of_clusters_mean.append(np.mean(num_clusters))
+    number_of_clusters_stdev.append(np.std(num_clusters))
+
+    rand_scores_all[i, :]  = rand_scores
+    rand_scores_mean.append(np.mean(rand_scores))
+    rand_scores_stdev.append(np.std(rand_scores))
+
+    if i > 0:
+        consistency_mean, consistency_std, consistency = compare_cluster_consistency(curr_assignments, prev_assigments)
+        cluster_consistency_mean.append(consistency_mean)
+        cluster_consistency_stdev.append(consistency_std)
+
+        cluster_consistency_all[i, :] = consistency
+
+    prev_assigments = curr_assignments
+np.save("overall_assignments", assignments_overall)
+
+np.save(networks_folder[:-1] + "_number_clusters.npy", number_clusters_all)
+np.save(networks_folder[:-1] + "_cluster_consistency_all.npy", cluster_consistency_all)
+np.save(networks_folder[:-1] + "_rand_scores_all.npy", rand_scores_all)
+
+dt = pd.to_datetime(dates_2)
+ts = pd.Series(rand_scores_mean, index=dt)
+fig = plt.figure()
+ax = ts.plot(yerr=rand_scores_stdev)
+plt.title("Rand Score")
+ax.set_ylim(0, 1)
+
+np.save("rand_scores_mean", rand_scores_mean)
+np.save("rand_scores_stdev", rand_scores_stdev)
+
+dt_2 = pd.to_datetime(dates)
+ts = pd.Series(cluster_consistency_mean, index=dt_2)
+fig = plt.figure()
+ax = ts.plot(yerr=cluster_consistency_stdev)
+plt.title("Clustering Consistency")
+ax.set_ylim(0, 1)
+
+np.save("cluster_consistency_mean", cluster_consistency_mean)
+np.save("cluster_consistency_stdev", cluster_consistency_stdev)
+
+ts = pd.Series(number_of_clusters_mean, index=dt)
+fig = plt.figure()
+ax = ts.plot(yerr=number_of_clusters_stdev)
+plt.title("Number of Clusters")
+ax.set_ylim(0, 25)
+
+np.save("num_clusters_mean", number_of_clusters_mean)
+np.save("num_clusters_stdev", number_of_clusters_stdev)
+
+save_open_figures("financial_networks_louvain_")
+plt.close('all')
