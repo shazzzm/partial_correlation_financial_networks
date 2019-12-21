61b8a14280c1a2ba4f801f2ae2294c234510f5da
Tristan Millington <tristan.millington@gmail.com>
2019-12-21 11:31:03 +0000

    Added normalization code

--- infer_networks.py
+++ infer_networks.py
@@ -43,6 +43,8 @@ df_2 = df_2.apply(pd.to_numeric)
 df_2 = np.log(df_2) - np.log(df_2.shift(1))
 X = df_2.values[1:, :]
 
+shrinkages = []
+
 window_size = 300
 slide_size = 30
 no_samples = X.shape[0]
@@ -51,9 +53,13 @@ no_runs = math.floor((no_samples - window_size)/ (slide_size))
 print("We're running %s times" % no_runs)
 
 X_new = X[0:window_size, :]
+ss = StandardScaler()
+X_new = ss.fit_transform(X_new)
 
 lw = LedoitWolf()
 lw.fit(X_new)
+shrinkages.append(lw.shrinkage_)
+
 corr = lw.covariance_
 
 prec = lw.precision_
@@ -88,8 +94,13 @@ for x in range(1, no_runs):
     print("Run %s" % x)
     X_new = X[x*slide_size:(x+1)*slide_size+window_size, :]
 
+    ss = StandardScaler()
+    X_new = ss.fit_transform(X_new)
+
     lw = LedoitWolf()
     lw.fit(X_new)
+
+    shrinkages.append(lw.shrinkage_)
     corr = covariance_matrix_to_corr(lw.covariance_)
 
     prec = lw.precision_
@@ -116,4 +127,7 @@ plt.title("Correlation Values")
 plt.figure()
 plt.hist(par_corr_values)
 plt.title("Partial Correlation Values")
+
+plt.figure()
+plt.plot(shrinkages, label="Shrinkages")   
 plt.show()
